dcfab691f66fe8d95b6ec7c4d5528ce783227287
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-03 00:53:01 +0300

    test: remove excedent chars

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming"
 str = f"{str[39:-63]} {str[107:-18]} {str[:-123]}"
 print(str)
