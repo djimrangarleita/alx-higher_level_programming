fd82998c52e9f340dca8cb144cd9c0a6aebd9445
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-03 19:47:09 +0300

    chore: print alphabets

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {number % 10}")
+print("Last digit of {} is {}".format(number, number % 10))
