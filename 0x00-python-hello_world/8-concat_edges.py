9ecd30dbc643fa4640b6c326416ff39a25799249
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-03 04:46:05 +0300

    test: debug

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming"
-str = f"{str[39:]}"
-# This is a comment
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str 
 print(str)
