e4b6f6ca31e6fe5f85d434203d46c98dc038d6e8
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-03 04:53:59 +0300

    test: debug

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str.format() 
+str.format(str[39:-63], str[107:-18], str[0:-123]) 
 print(str)
