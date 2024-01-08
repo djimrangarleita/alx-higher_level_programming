f9f3750ddbea6e9e92c8ab77e97251a5643681e0
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-08 21:34:39 +0300

    chore: sum tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return (tuple_a)
+    tuple_a, tuple_b = unpack_tuple(tuple_a), unpack_tuple(tuple_b)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+
+def unpack_tuple(tup=()):
+    if len(tup) < 1:
+        return (0, 0)
+    if len(tup) < 2:
+        return (tup[0], 0)
+    return (tup[0], tup[1])
