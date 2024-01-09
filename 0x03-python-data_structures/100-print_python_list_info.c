fe5f9ff3a99761ee63170f0e88807b4e50d26b42
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-09 03:35:08 +0300

    fix: remove cast for iterator

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,15 +8,14 @@
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *lp;
-	long int listsize, i;
+	int i;
 
 	lp = (PyListObject *)p;
-	listsize = lp->ob_base.ob_size;
 
-	printf("[*] Size of the Python List = %ld\n", listsize);
+	printf("[*] Size of the Python List = %ld\n", lp->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", lp->allocated);
-	for (i = 0; i < listsize; i++)
+	for (i = 0; i < lp->ob_base.ob_size; i++)
 	{
-		printf("Element %ld: %s\n", i, lp->ob_item[i]->ob_type->tp_name);
+		printf("Element %d: %s\n", i, lp->ob_item[i]->ob_type->tp_name);
 	}
 }
