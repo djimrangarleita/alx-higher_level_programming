31c324b64f1516cb4ca5858a312b151beddd80af
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-09 03:31:06 +0300

    fix: ob_size is %ld

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
-#include "Python.h"
 #include <stdio.h>
+#include "Python.h"
 
 /**
  * print_python_list_info - print info about python lists
@@ -8,7 +8,15 @@
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *lp;
+	long int listsize, i;
 
 	lp = (PyListObject *)p;
+	listsize = lp->ob_base.ob_size;
+
+	printf("[*] Size of the Python List = %ld\n", listsize);
 	printf("[*] Allocated = %ld\n", lp->allocated);
+	for (i = 0; i < listsize; i++)
+	{
+		printf("Element %ld: %s\n", i, lp->ob_item[i]->ob_type->tp_name);
+	}
 }
