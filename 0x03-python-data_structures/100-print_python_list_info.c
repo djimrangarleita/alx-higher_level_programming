ba425ce89d05b4f6455cc356259577068ba93ff0
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-09 03:46:45 +0300

    fix: import python first

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "Python.h"
+#include <stdio.h>
 
 /**
  * print_python_list_info - print info about python lists
@@ -8,10 +8,15 @@
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *lp;
+	int listsize, i;
 
 	lp = (PyListObject *)p;
+	listsize = lp->ob_base.ob_size;
 
-	printf("[*] Size of the Python List = %ld\n", lp->ob_base.ob_size);
+	printf("[*] Size of the Python List = %d\n", listsize);
 	printf("[*] Allocated = %ld\n", lp->allocated);
-	printf("Element 0\n");
+	for (i = 0; i < listsize; i++)
+	{
+		printf("Element %d: %s\n", i, lp->ob_item[i]->ob_type->tp_name);
+	}
 }
