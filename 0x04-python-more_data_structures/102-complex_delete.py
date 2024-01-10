25aaeeced9a58aeece1e5d4053a0a4df7e2ed9f4
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-11 00:31:56 +0300

    fix: raise matrix to pow 2, x**2 instead of x*2

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary:
-        for k in list(a_dictionary):
-            if a_dictionary[k] == value:
-                del a_dictionary[k]
+    for k in list(a_dictionary):
+        if a_dictionary[k] == value:
+            del a_dictionary[k]
     return a_dictionary
