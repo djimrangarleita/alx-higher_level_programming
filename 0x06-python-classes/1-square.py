6b5d99cb2c70f2f41c560a2f5e9d1bbcf6f003da
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-24 03:20:52 +0300

    chore: create a class square with private ppt size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class named Square"""
+
+
+class Square:
+    """Square class that does nothing that has an instance attr"""
+
+    def __init__(self, size):
+        """Initialize a private instance attr, size"""
+        self.__size = size
