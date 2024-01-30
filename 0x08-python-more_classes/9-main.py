ac7233a128e708c2daff0f7857fc96f9a261b378
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-30 03:16:40 +0300

    fix: initialize width first

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -4,3 +4,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
+
+print('------------')
+
+
+my_square2 = Rectangle.square(0)
+print("Area: {} - Perimeter: {}".format(my_square2.area(), my_square2.perimeter()))
+print(my_square2)
+
+print('---------------')
+
+my_square3 = Rectangle.square('I am')
+print("Area: {} - Perimeter: {}".format(my_square3.area(), my_square3.perimeter()))
+print(my_square3)
