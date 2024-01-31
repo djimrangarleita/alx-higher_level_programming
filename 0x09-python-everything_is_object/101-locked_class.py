82354cbc56051062c01218c8c2bed4d20fee8bd3
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-31 04:17:13 +0300

    fix: pycode

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-
 """Module that will create a looked class"""
+
+
 class LockedClass(object):
     """This class is locked and accept only first_name as dynamic attr"""
     __slots__ = ['first_name']
