12c0c68b513c06cd3a934fb931e8e48ed2c7982d
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-06 03:45:53 +0300

    fix: add if condition for type(obj) == a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,6 @@
 
 def inherits_from(obj, a_class):
     """Check if obj inherits from a_class"""
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
