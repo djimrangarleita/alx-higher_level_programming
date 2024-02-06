9a731298aedb63fa5650753552fd4ea5d4e6b24a
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-07 01:52:51 +0300

    chore: append to a file or create and write to

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module used to write to a file"""
+
+def append_write(filename="", text=""):
+    """Append text to a file"""
+    nchars = 0
+    with open(filename, 'a', encoding='utf-8') as f:
+        nchars = f.write(text)
+    return nchars
