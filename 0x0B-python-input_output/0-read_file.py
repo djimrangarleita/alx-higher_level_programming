e65c9b407eba3415662ed2a193f92d0526f70d5d
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-07 01:27:43 +0300

    fix: remove last \n

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function that reads a file"""
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
-        print(content)
+        print(content.rstrip())
