5accee5b5cb1098eb8c74c320fea60ee6850f16e
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-07 01:26:04 +0300

    Initial commit, readme and task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with one function"""
+
+
+def read_file(filename=""):
+    """Function that reads a file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+        print(content)
