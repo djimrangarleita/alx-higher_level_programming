502480eb98128b57ef94e8182e4f35a6ec2e7ffc
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-08 00:08:54 +0300

    chore: append after a searched char

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """Module comment"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Please implement me"""
-    pass
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    line_number = 1
+    for line in lines:
+        if search_string in line:
+            lines.insert(line_number, new_string)
+        line_number += 1
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(lines)
