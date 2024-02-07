c2101f8712c282d961d08aa2e22d3b01ce76b408
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-07 05:55:24 +0300

    chore: empty files

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module comment"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """Please implement me"""
+    pass
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module comment"""
+
+def pascal_triangle(n):
+    """Pascal triangle function"""
+    pass
