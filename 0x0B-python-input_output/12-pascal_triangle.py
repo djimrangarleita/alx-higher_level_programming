3693a1ae11187b3be56672165c7a5701b6ba40ff
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-08 04:52:36 +0300

    chore: pascal triangle list

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Module comment"""
 
+
 def pascal_triangle(n):
     """Pascal triangle function"""
-    pass
+    result = []
+    for i in range(n):
+        result.append([int(x) for x in str(11**i)])
+    return result
