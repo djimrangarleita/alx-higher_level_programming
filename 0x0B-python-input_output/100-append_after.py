39d36cd510c40a09f0670ead4ce7bb9b22eef121
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-08 04:04:25 +0300

    fix: change reading strategy

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,13 +5,11 @@
 def append_after(filename="", search_string="", new_string=""):
     """Please implement me"""
     with open(filename, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-
-    line_number = 1
-    for line in lines:
-        if search_string in line:
-            lines.insert(line_number, new_string)
-        line_number += 1
+        data = ""
+        for line in f:
+            data += line
+            if search_string in line:
+                data += new_string
 
     with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(lines)
+        f.write(data)
