613ee1ff7b496e319aebcf569ea79628cc047f65
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-10 15:55:48 +0300

    chore: add property validation for class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(base.Base):
 
     @width.setter
     def width(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
         self.__width = width
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(base.Base):
 
     @height.setter
     def height(self, height):
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
         self.__height = height
 
     @property
@@ -49,6 +57,10 @@ class Rectangle(base.Base):
 
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = x
 
     @property
@@ -58,4 +70,8 @@ class Rectangle(base.Base):
 
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
         self.__y = y
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -5,7 +5,7 @@ Module used to test the rectangle class
 from models.rectangle import Rectangle
 import unittest
 
-class TestRectangleClass(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
     """Test suite for the Rectangle class"""
 
     def test_instantiation_with_id(self):
@@ -25,12 +25,30 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(rec1.width, 12)
         self.assertEqual(rec2.width, 11)
 
+    def test_instantiation_with_invalid_width(self):
+        """Test instantiation with invalid width should raise exception"""
+        with self.assertRaises(TypeError):
+            Rectangle('10', 3)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 5)
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 5)
+
     def test_instantiation_with_valid_height(self):
         """Test instantiation with height works"""
         rec1 = Rectangle(12, 3)
         rec2 = Rectangle(11, 5)
         self.assertEqual(rec1.height, 3)
         self.assertEqual(rec2.height, 5)
+
+    def test_instantiation_with_invalid_height(self):
+        """Test instantiation with invalid height should raise exception"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, '3')
+        with self.assertRaises(ValueError):
+            Rectangle(2, -1)
+        with self.assertRaises(ValueError):
+            Rectangle(2, 0)
 
     def test_instantiation_with_valid_x(self):
         """Test instantiation with x"""
@@ -39,9 +57,25 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(rec1.x, 0)
         self.assertEqual(rec2.x, 50)
 
+    def test_instantiation_with_invalid_x(self):
+        """Test instantiation with invalid width should raise exception"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, 3, '12')
+        with self.assertRaises(ValueError):
+            Rectangle(10, 5, -1)
+        self.assertEqual(Rectangle(10, 4, 0).x, 0)
+
     def test_instantiation_with_valid_y(self):
         """Test instantiation with y"""
         rec1 = Rectangle(12, 3)
         rec2 = Rectangle(12, 3, 5, 50, 9)
         self.assertEqual(rec1.y, 0)
         self.assertEqual(rec2.y, 50)
+
+    def test_instantiation_with_invalid_y(self):
+        """Test instantiation with invalid y should raise exception"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, 3, 12, '10')
+        with self.assertRaises(ValueError):
+            Rectangle(10, 5, 1, -1)
+        self.assertEqual(Rectangle(10, 4, 2, 0).y, 0)
