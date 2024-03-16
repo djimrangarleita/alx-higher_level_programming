f7fd8cc29865bf8acf8308926ee1d80361cf957f
Djimra NGARLEITA <dngarleita@gmail.com>
2024-03-16 18:46:00 +0300

    chore: convert db to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Convert the db and it's table first_table to utf8mb4, utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0 
+	CHARACTER SET = utf8mb4 
+	COLLATE = utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
+ALTER TABLE first_table
+	CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+	MODIFY COLUMN name VARCHAR(256)
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
