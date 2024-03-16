de03ee2cc755df1e7f04dfed65b8cd9f27798070
Djimra NGARLEITA <dngarleita@gmail.com>
2024-03-16 19:22:19 +0300

    chore: compute temperatures table

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Average temp by city
+SELECT city, AVG(value) as avg_temp
+	FROM temperatures
+	GROUP BY city
+	ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Average temp by city
+SELECT city, AVG(value) as avg_temp
+	FROM temperatures
+	WHERE month = 7 OR month = 8
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
