eedfa60f96331fa5fd8a2f53a681965d2e314286
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-09 00:44:25 +0300

    chore: do task 1 to 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length === 2) {
+  console.log('No argument');
+}
+if (argv.length === 3) {
+  console.log('Argument found');
+}
+if (argv.length > 3) {
+  console.log('Arguments found');
+}
