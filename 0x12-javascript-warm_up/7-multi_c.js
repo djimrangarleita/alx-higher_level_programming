59937591071ef159bc11f55536db326faf7f5014
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-09 01:55:10 +0300

    chore: do tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (isNaN(argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  let occur = Number(argv[2]);
+  while (occur > 0) {
+    console.log('C is fun');
+    occur--;
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (isNaN(argv[2])) {
+  console.log('Missing size');
+} else {
+  const size = Number(argv[2]);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
