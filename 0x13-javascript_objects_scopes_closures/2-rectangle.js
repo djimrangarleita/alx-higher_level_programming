780c294dc0698cc2b2e5ef75eea12c7acd31efd4
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-13 01:41:59 +0300

    chore: create instance attributes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number(w) && Number(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
