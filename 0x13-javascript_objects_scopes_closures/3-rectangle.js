5e95fccf65fbf08d938227ceebaa7d43802ac223
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-13 02:08:05 +0300

    chore: add instance methods and inheritance

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number(w) > 0 && Number(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const x = 'X'.repeat(this.width);
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number(w) > 0 && Number(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const x = 'X'.repeat(this.width);
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(x);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+module.exports = Rectangle;
