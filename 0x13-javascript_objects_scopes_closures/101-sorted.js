4e1b69590cf1370c6c6a3875ce91b6f46519c0db
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-14 02:31:12 +0300

    chore: do task 101, sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
 
-const dict = require('101-data').dict;
+const dict = require('./101-data').dict;
+
+const keys = Object.keys(dict).sort((a, b) => a - b);
+const myObj = {};
+
+keys.forEach(key => {
+  if (!myObj[dict[key]]) {
+    myObj[dict[key]] = [];
+  }
+  myObj[dict[key]].push(key);
+});
+
+console.log(myObj);
