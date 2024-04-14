7c34a18fac9124697f73bc7c7fd78ab391e71b3f
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-15 02:48:31 +0300

    chore: filter cities by state name arg

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+List all cities from db hbtn_0e_0_usa by states
+"""
+import MySQLdb
+import sys
+
+
+def get_all_cities_by_state_name():
+    """Read and list of cities in db"""
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=uname,
+                           passwd=password, db=dbname, charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name AS state_name\
+            FROM cities JOIN states ON cities.state_id = states.id\
+            WHERE BINARY states.name = %s", (state_name,))
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    get_all_cities_by_state_name()
