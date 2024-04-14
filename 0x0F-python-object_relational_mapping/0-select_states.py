d00c492de4b2e55a34ba4d575559b223836132e1
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-15 00:38:27 +0300

    fix: execute code only if run as main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,25 @@ List all states from db hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-uname = sys.argv[1]
-password = sys.argv[2]
-dbname = sys.argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=uname,
-                       passwd=password, db=dbname, charset="utf8")
+def get_all_states():
+    """Read and list states in db"""
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
 
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=uname,
+                           passwd=password, db=dbname, charset="utf8")
 
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    get_all_states()
