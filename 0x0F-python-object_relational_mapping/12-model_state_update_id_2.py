f765c6220216105702370873cce1972f88e84d91
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-16 02:19:48 +0300

    chore: update state with id=2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def update_state():
+    """Create single state"""
+    if (len(sys.argv) != 4):
+        return
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = 'New Mexico'
+        session.commit()
+
+
+if __name__ == "__main__":
+    update_state()
