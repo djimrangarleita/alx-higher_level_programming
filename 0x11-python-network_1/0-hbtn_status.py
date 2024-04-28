5b597ffc4281a20f80536f6bc9db91e98ac2386c
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-28 21:54:20 +0300

    chore(#1): print response header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,7 @@ import urllib.request
 
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
    print('Body response:')
-   print('\t-type: {}'.format(response.read().decode('utf-8')))
-   print('\t-content: {}'.format(response.read()))
-   print('\t-utf8 content: {}'.format(response.headers['Content-Type']))
+   print('\t- type: {}'.format(type(response.read())))
+   print('\t- content: {}'.format(response.read().decode('utf-8')))
+   print('\t- utf8 content: {}'.format(response.msg))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 """Change me"""
+import urllib.request
+import sys
+
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    print(response.headers['X-Request-Id'])
