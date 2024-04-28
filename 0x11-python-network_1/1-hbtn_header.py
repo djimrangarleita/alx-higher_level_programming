bc5888cf002125ee8fdef5492250bfb2f080f881
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-28 22:06:32 +0300

    fix(#1): requirements

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """Print request header, key X-Request-Id"""
+
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+def print_header():
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
+
+
+if __name__ == '__main__':
+    print_header()
