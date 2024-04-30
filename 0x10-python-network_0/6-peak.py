ab2b0e8909eb3f500b272c7848c4a17fa0175962
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 01:04:24 +0300

    fix(#6): improve selection allgo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,17 @@
 """ Find a peak in a list of integers """
 
 
-def find_peak(list_of_integers):
+def find_peak(ints):
     """ Find peak in a list of integers """
-    if not list_of_integers:
+    if not ints:
         return None
-    return max(list_of_integers)
+    if len(ints) <= 2:
+        return max(ints)
+    if ints[0] > ints[1]:
+        return ints[0]
+    i = 1
+    while (i < len(ints) - 1):
+        if ints[i] > ints[i - 1] and ints[i + 1] < ints[i]:
+            return ints[i]
+        i += 1
+    return ints[0]
