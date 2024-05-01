fd41e6f9dbc78ded5da899410818052ae03e86cc
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 15:14:50 +0300

    chore(#3): send request and manage HTTPError, fix #0 output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   print('Body response:')
-   print('\t- type: {}'.format(type(response.read())))
-   print('\t- content: {}'.format(response.read().decode('utf-8')))
-   print('\t- utf8 content: {}'.format(response.msg))
+    content = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(content.decode('utf-8')))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Print request body or status code in case of error"""
+
+import urllib.request
+import sys
+
+
+def print_request():
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            content = response.read()
+            print(content.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
+
+
+if __name__ == '__main__':
+    print_request()
