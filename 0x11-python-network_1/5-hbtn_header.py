47bab0b68322b1b397892db4a686e9c686dd1d22
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 15:44:47 +0300

    chore(#5): print request header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Print request header, key X-Request-Id"""
+
+import requests
+import sys
+
+
+def print_header():
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
+
+
+if __name__ == '__main__':
+    print_header()
