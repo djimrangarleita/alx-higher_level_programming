27fb3ee56a1f5656f25331df9e10b7cac7f6f1d5
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 16:22:36 +0300

    chore(#8): send request with query param

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Print response or custom message"""
+
+import requests
+import sys
+
+
+def print_response():
+    r = requests.post('http://0.0.0.0:5000/search_user',
+                      params={'q': sys.argv[1]})
+    try:
+        r = r.json()
+        if not r:
+            print('No result')
+            return
+        print(f'[{r.id}] {r.name}')
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    print_request()
