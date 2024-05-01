0995e0a3864b6aa527944e5ef3a4f28634ddc5d7
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 18:33:07 +0300

    chore(#9): print my github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Request github public info and print id"""
+
+import requests
+import sys
+
+
+def request_info():
+    r = requests.get('https://api.github.com/user', auth=(sys.argv[1],
+                                                          sys.argv[2]))
+    r = r.json()
+    print(r.get('id'))
+
+
+if __name__ == '__main__':
+    request_info()
