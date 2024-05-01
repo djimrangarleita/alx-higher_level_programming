11211fc769e602c7a335f8031e84ad27ce6f9452
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 16:01:19 +0300

    chore(#7): print response body or status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Print request body or status code in case of error"""
+
+import requests
+import sys
+
+
+def print_request():
+    try:
+        r = requests.get(sys.argv[1])
+        r.raise_for_status()
+        print(r.text)
+    except requests.exceptions.HTTPError:
+        print('Error code:', r.status_code)
+
+
+if __name__ == '__main__':
+    print_request()
