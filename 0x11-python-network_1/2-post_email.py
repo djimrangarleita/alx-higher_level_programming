91ab3e20f9c9a813ff0c7feb01110dc4d00910fd
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 14:47:39 +0300

    chore(#2): send post request and print response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Send post request and print response"""
+
+import urllib.request
+import sys
+
+
+def send_email():
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print(content.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    send_email()
