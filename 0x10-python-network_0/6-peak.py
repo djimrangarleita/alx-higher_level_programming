4927143a48dddde4bd70d434dc263d2999679594
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 12:36:57 +0300

    fix(#6): update peak condition for first elt in list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,7 @@ def find_peak(ints):
         return None
     if len(ints) <= 2:
         return max(ints)
-    if ints[0] > ints[1]:
+    if ints[0] >= ints[1]:
         return ints[0]
     i = 1
     while (i < len(ints) - 1):
