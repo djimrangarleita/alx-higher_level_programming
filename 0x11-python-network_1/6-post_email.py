773d726d2935557599bab727d9bb47d7bf3d2506
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 15:51:06 +0300

    chore(#6): post email address to url via requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Send post request and print response"""
+
+import requests
+import sys
+
+
+def send_email():
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
+
+
+if __name__ == '__main__':
+    send_email()
