dacc610c4b32b6851299e811bf054cc32f55ecae
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-01 15:38:46 +0300

    chore(#4): use request lib to send request and print response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-"""Change me"""
+"""Send http request with the request lib"""
+import requests
+
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print('Body response:')
+print('\t- type:', type(r.text))
+print('\t- content:', r.text)
