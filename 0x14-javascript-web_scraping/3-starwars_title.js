2b4783dba9ef5330ed6c358d867ac79abfb404b2
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-01 23:24:07 +0300

    chore(#3): print movie title from json response

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+const { argv } = require('node:process');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+
+request(url, (err, response, body) => {
+  if (response) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.error(err);
+  }
+});
