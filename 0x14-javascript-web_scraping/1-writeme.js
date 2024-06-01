48b0e3e6200b9758caa4567c455ff954679586fe
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-01 22:55:11 +0300

    chore(#1): write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const fs = require('node:fs');
+const { argv } = require('node:process');
+
+const path = argv[2];
+const content = argv[3];
+
+fs.writeFile(path, content, err => {
+  if (err) {
+    console.log(err);
+  }
+});
