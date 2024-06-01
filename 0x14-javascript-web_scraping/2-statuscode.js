88680f0a2d655c3c092401e64cb5382838f094d3
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-01 23:09:30 +0300

    chore(#2): read http status code from response

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+const { argv } = require('node:process');
+
+const url = argv[2];
+
+request(url, (err, response, body) => {
+  if (response) {
+    console.log('code:', response.statusCode);
+  } else {
+    console.error(err);
+  }
+});
