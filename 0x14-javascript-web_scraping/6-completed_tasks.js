23f4021926ae9a8893d195e237ebfd35b05dd686
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-02 00:32:19 +0300

    chore(#6): count completed tasks per user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+const { argv } = require('node:process');
+
+const url = argv[2];
+
+request(url, (err, response, body) => {
+  const usersList = {};
+  if (response) {
+    const result = JSON.parse(body);
+    result.forEach(user => {
+      if (user.completed) {
+        const cUserId = String(user.userId);
+        if (!(cUserId in usersList)) {
+          usersList[cUserId] = 0;
+        }
+        usersList[cUserId]++;
+      }
+    });
+    console.log(usersList);
+  } else {
+    console.error(err);
+  }
+});
