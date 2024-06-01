5aebbc72ea5588d9d854de814c9cef9639b0f5c1
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-02 00:03:03 +0300

    chore(#5): write response body to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const { argv } = require('node:process');
+const fs = require('node:fs');
+
+const url = argv[2];
+const path = argv[3];
+
+request(url, (err, response, body) => {
+  if (response) {
+    fs.writeFile(path, body, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else {
+    console.error(err);
+  }
+});
