b112421b0ae7157c124516e94c390f3e31a95cb2
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-01 23:50:11 +0300

    chore(#4): count character in movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url, (err, response, body) => {
+  if (response) {
+    let characterCount = 0;
+    const films = JSON.parse(body);
+    films.results.forEach(result => {
+      result.characters.forEach(character => {
+        if (character.includes('/18/')) {
+          characterCount++;
+        }
+      });
+    });
+    console.log(characterCount);
+  } else {
+    console.error(err);
+  }
+});
